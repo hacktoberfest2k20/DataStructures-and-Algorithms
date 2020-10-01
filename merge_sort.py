727d68bcf0f258d7643b938017a92c4e162c9505
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-10-01 21:16:23 +0700

    add merge sort in python

---
+++ merge_sort.py
@@ -0,0 +1,54 @@
+def merge_sort(array, left_index, right_index):
+    if left_index >= right_index:
+        return
+
+    middle = (left_index + right_index)//2
+    merge_sort(array, left_index, middle)
+    merge_sort(array, middle + 1, right_index)
+    merge(array, left_index, right_index, middle)
+
+def merge(array, left_index, right_index, middle):
+    left_copy = array[left_index:middle + 1]
+    right_copy = array[middle+1:right_index+1]
+
+    left_copy_index = 0
+    right_copy_index = 0
+    sorted_index = left_index
+
+    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
+
+        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
+            array[sorted_index] = left_copy[left_copy_index]
+            left_copy_index = left_copy_index + 1
+        else:
+            array[sorted_index] = right_copy[right_copy_index]
+            right_copy_index = right_copy_index + 1
+
+        sorted_index = sorted_index + 1
+
+    while left_copy_index < len(left_copy):
+        array[sorted_index] = left_copy[left_copy_index]
+        left_copy_index = left_copy_index + 1
+        sorted_index = sorted_index + 1
+
+    while right_copy_index < len(right_copy):
+        array[sorted_index] = right_copy[right_copy_index]
+        right_copy_index = right_copy_index + 1
+        sorted_index = sorted_index + 1
+
+def main():
+    list = []
+    n = int(input("Enter the number of elements: "))
+    for i in range(0,n):
+        element = int(input())
+        list.append(element)
+    
+    print("Unsorted list: ")
+    print(list)
+    
+    merge_sort(list, 0, n)
+    print("Sorted list: ")
+    print(list)
+
+
+main()
