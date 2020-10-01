90764c4db29bc15dec2b3e26db84bc7b8eb4eb79
Rishabh <rishabh05092001@gmail.com>
2020-10-01 19:12:19 +0530

    added code for gcd problem.

---
+++ gcd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+long long int gcd(long long int a,long long int b)
+{
+	if(b==0)
+	return a;
+	else
+	{
+		a = a % b;
+		return gcd(b,a); /*Famous euclid theorem,property of gcd is used here*/
+	}
+}
+
+int main()
+{
+	long long int a,b,n;
+	scanf("%lld %lld",&a,&b);
+	n = gcd(a,b);
+	printf("%lld",n);
+	return 0;
+}
