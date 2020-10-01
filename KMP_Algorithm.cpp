fccd509a410f351c218290f24d407ffcab416da9
rustiever <sharanzoff@gmail.com>
2020-10-01 18:31:07 +0530

    added KMP algorithm

---
+++ KMP_Algorithm.cpp
@@ -0,0 +1,70 @@
+/*
+ Pattern Search using Knuth–Morris–Pratt Algorithm (KMP)
+ Time Complexity:  Θ(m(preprocessing) + n (matching))
+ Auxiliary Space: O(m)
+ */
+#include <iostream>
+#include <cstring>
+using namespace std;
+void build(string pattern, int f[])
+{
+    int m = pattern.length(), k;
+    f[0] = -1;
+    for (int i = 1; i < m; i++)
+    {
+        k = f[i - 1];
+        while (k >= 0)
+        {
+            if (pattern[k] == pattern[i - 1])
+                break;
+            else
+                k = f[k];
+        }
+        f[i] = k + 1;
+    }
+}
+ 
+//check whether target string contains pattern 
+bool check(string pattern, string target)
+{
+    int m = pattern.length();
+    int n = target.length();
+    int f[m];     
+    build(pattern, f);     
+    int i = 0;
+    int k = 0;        
+    while (i < n)
+    {
+        if (k == -1)
+        {
+            i++;
+            k = 0;
+        }
+        else if (target[i] == pattern[k])
+        {
+            i++;
+            k++;
+            if (k == m)
+                return 1;
+        }
+        else
+            k = f[k];
+    }
+    return 0;
+}
+ 
+int main()
+{
+    string tar = "Hacktoberfest2k20-Official";
+    string pat = "line";
+    if (check(pat, tar))
+        cout<<"'"<<pat<<"' found in string '"<<tar<<"'"<<endl;
+    else
+        cout<<"'"<<pat<<"' not found in string '"<<tar<<"'"<<endl;
+    pat = "cial";
+    if (check(pat, tar))
+        cout<<"'"<<pat<<"' found in string '"<<tar<<"'"<<endl;
+    else
+        cout<<"'"<<pat<<"' not found in string '"<<tar<<"'"<<endl;
+    return 0;
+}
