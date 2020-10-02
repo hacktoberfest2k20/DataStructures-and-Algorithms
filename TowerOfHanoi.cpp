05f39c700b22a5ec338d95d51fc4273ca600781f
garvit170 <garvitgulati47@gmail.com>
2020-10-02 10:59:52 +0530

    Added comments to tower of hanoi

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -1,3 +1,16 @@
+/*
+Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. 
+The objective of the puzzle is to move the entire stack to another rod, obeying the following 
+simple rules:
+1) Only one disk can be moved at a time.
+2) Each move consists of taking the upper disk from one of the stacks and placing it on top of 
+another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+3) No disk may be placed on top of a smaller disk.
+
+*/
+
+//Time Complexity O(2^n)
+
 #include <iostream>
 using namespace std;
 
