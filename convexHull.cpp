8d3c124e2d8bf6bc860c3bd4f856164d25e50f95
Harikesh Pallantla <p.harikesh409@gmail.com>
2020-10-02 13:17:26 +0530

    Updated according to the changes suggested.

--- convexHull.cpp
+++ convexHull.cpp
@@ -1,6 +1,33 @@
-#include <iostream> 
-#include <stack> 
-#include <stdlib.h> 
+/** 
+* Reference: http://www.dcs.gla.ac.uk/~pat/52233/slides/Hull1x1.pdf
+==================================================================================================================================================
+1) Find the bottom-most point by comparing y coordinate of all points. 
+If there are two points with the same y value, then the point with smaller x coordinate value is considered. 
+Let the bottom-most point be P0. Put P0 at first position in output hull.
+2) Consider the remaining n-1 points and sort them by polar angle in counterclockwise order around points[0]. 
+If the polar angle of two points is the same, then put the nearest point first.
+3 After sorting, check if two or more points have the same angle. If two more points have the same angle, 
+then remove all same angle points except the point farthest from P0. Let the size of the new array be m.
+4) If m is less than 3, return (Convex Hull not possible).
+5) Create an empty stack ‘S’ and push points[0], points[1] and points[2] to S.
+6) Process remaining m-3 points one by one. Do following for every point ‘points[i]’
+        6.1) Keep removing points from stack while orientation of following 3 points is not counterclockwise (or they don’t make a left turn).
+            a) Point next to top in stack
+            b) Point at the top of stack
+            c) points[i]
+         6.2) Push points[i] to S
+7) Print contents of S
+==================================================================================================================================================
+* Time Complexity: Let n be the number of input points. The algorithm takes O(nLogn) time if we use a O(nLogn) sorting algorithm.
+* Step 1: Finding the bottom-most point takes O(n) time. 
+* Step 2: Sorting the points takes O(nLogn) time. 
+* Step 3: Every element is pushed and popped at most one time which takes O(n) time.
+* Step 4: Process points one by one takes O(n) time. 
+* Assuming that the stack operations take O(1) time. Overall complexity is O(n) + O(nLogn) + O(n) + O(n) which is O(nLogn)
+==================================================================================================================================================
+*/
+
+#include <bits/stdc++.h>
 using namespace std; 
   
 struct Point 
