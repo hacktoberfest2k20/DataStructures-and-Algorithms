72c151a36b66baab03fd411398598facba83e297
Ayush Aryan <aryanayush012@gmail.com>
2022-10-05 11:56:50 +0530

    solved  Tower of Hanoi in C++

---
+++ Tower_of_Hanoi.cpp
@@ -0,0 +1,29 @@
+
+// in this approach we use recursion and take the help of the auxilary rod to move all the discs from
+//one rod to another
+// our refrenced auxilary rod changes everytime and we call the function recursively
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void towerOfHanoi(int n, char from_rod, char to_rod,
+                  char aux_rod)
+{
+    if (n == 0) {
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod
+         << " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+ 
+
+int main()
+{
+    int N = 3;
+ 
+    
+    towerOfHanoi(N, 'A', 'C', 'B');
+    return 0;
+}
