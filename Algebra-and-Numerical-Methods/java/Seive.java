c98973dca67bbaca4dbe6aada6e97f4c84e1c85e
Ayush Shrivastava <ayush_1901cb14@iitp.ac.in>
2022-10-05 12:45:18 +0530

    algebra/sieve: fix file name

--- Algebra-and-Numerical-Methods/java/Seive.java
+++ Algebra-and-Numerical-Methods/java/Seive.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class SievePrimeFactors  {
+public class Sieve  {
    public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a number");
